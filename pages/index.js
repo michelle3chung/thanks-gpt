3ae9a0e81b0a03ba29f50c7c618f036fefef753b
Matt Rea <matthewjamesrea@gmail.com>
2023-01-04 20:22:29 -0600

    grid works now!

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,30 @@
-import { Grid, makeStyles, TextField } from '@mui/material';
+import { Grid, TextField } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 
-// const useStyles = makeStyles({
-//   root: {
-//     height: '1000vh',
-//   },
-//   left: {
-//     backgroundColor: '#1F1F1F',
-//   },
-//   right: {
-//     backgroundColor: '#FFFFFF',
-//   },
-// });
+const useStyles = makeStyles({
+  root: {
+    height: '1000vh',
+  },
+  left: {
+    backgroundColor: '#1F1F1F',
+  },
+  right: {
+    backgroundColor: '#FFFFFF',
+  },
+});
 
 export default function SplitScreen(props) {
-  // const classes = useStyles();
+  const classes = useStyles();
 
   return (
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-    // <Grid container className={classes.root}>
-    //   <Grid item xs={6} className={classes.left}>
-    //     Left Side
-    //   </Grid>
-    //   <Grid item xs={6} className={classes.right}>
-    //     Right Side
-    //   </Grid>
-    // </Grid>
+      // <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    <Grid container className={classes.root}>
+      <Grid item xs={6} className={classes.left}>
+        Left Side
+      </Grid>
+      <Grid item xs={6} className={classes.right}>
+        Right Side
+      </Grid>
+    </Grid>
   );
 }
