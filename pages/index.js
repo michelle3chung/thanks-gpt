486599977c318d157857ca11c61679a6a9105b41
Matt Rea <matthewjamesrea@gmail.com>
2023-01-04 20:14:45 -0600

    ok it works...kind of

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,29 @@
-import { Grid, makeStyles } from '@material-ui/core';
+import { Grid, makeStyles, TextField } from '@mui/material';
 
-const useStyles = makeStyles({
-  root: {
-    height: '100vh',
-  },
-  left: {
-    backgroundColor: '#1F1F1F',
-  },
-  right: {
-    backgroundColor: '#FFFFFF',
-  },
-});
+// const useStyles = makeStyles({
+//   root: {
+//     height: '1000vh',
+//   },
+//   left: {
+//     backgroundColor: '#1F1F1F',
+//   },
+//   right: {
+//     backgroundColor: '#FFFFFF',
+//   },
+// });
 
 export default function SplitScreen(props) {
-  const classes = useStyles();
+  // const classes = useStyles();
 
   return (
-    <Grid container className={classes.root}>
-      <Grid item xs={6} className={classes.left}>
-        Left Side
-      </Grid>
-      <Grid item xs={6} className={classes.right}>
-        Right Side
-      </Grid>
-    </Grid>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    // <Grid container className={classes.root}>
+    //   <Grid item xs={6} className={classes.left}>
+    //     Left Side
+    //   </Grid>
+    //   <Grid item xs={6} className={classes.right}>
+    //     Right Side
+    //   </Grid>
+    // </Grid>
   );
 }
